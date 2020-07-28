b2cee6580e69608f14d5a38083747a34ccd805c4
Ernesto Canquiz <fdsoil123@gmail.com>
2020-07-28 16:57:12 -0400

    3. Manipulación del DOM (12 Expresiones)

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   #app
     img(src="./assets/logo.png")
     h1 {{ msg }}
+    p {{ 1 + 1 }}
+    p {{ 'Hola' + 'Mundo' }}
+    p {{ person.name }}
+    p {{ person.name.toUpperCase() }}
+    p {{ JSON.stringify(person) }}
+    p {{ true ? 'true' : 'false' }}
 </template>
 
 <script>
@@ -9,7 +15,10 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Vue!'
+      msg: 'Hola Vue!',
+      person: {
+          name: 'Juan'
+      }
     }
   }
 }
