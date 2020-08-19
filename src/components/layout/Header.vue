d3f74a87afe723921246c879f82012e21711e9fd
Ernesto Canquiz <fdsoil123@gmail.com>
2020-08-19 07:44:05 -0400

    23 Component & 24 Creating components

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong Radio Platzi Music
+          .nav-right.nav-menu
+    .hero-body
+      .container.has-text-centered
+        h1.title Platzi Music
+        h2.subtitle Canciones que están Vue-nisimas!
+</template>
