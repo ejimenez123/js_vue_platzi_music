a9edab454e1a83f18e87e03d503d2d2c209c063d
Ernesto Canquiz <fdsoil123@gmail.com>
2020-09-18 09:34:58 -0400

    30 Communication between Generic Components - Event Bus and Plugins

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -11,4 +11,14 @@
       .container.has-text-centered
         h1.title Platzi Music
         h2.subtitle Canciones que están Vue-nisimas!
+        pm-player
 </template>
+
+<script>
+  import PmPlayer from '@/components/Player.vue'
+
+  export default {
+    components: {PmPlayer}
+  }
+
+</script>
